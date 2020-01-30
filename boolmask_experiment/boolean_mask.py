6ac17279af45b844e246779910ef71900f859228
stealthness <stevewestinlondon@gmail.com>
2020-01-30 17:13:19 +0000

    bug hunt partial found

--- boolmask_experiment/boolean_mask.py
+++ boolmask_experiment/boolean_mask.py
@@ -10,8 +10,20 @@ def get_mask(size, **kwargs):
             return np.array([True] * size)
         if kwargs['type'] == 'empty':
             return np.array([False] * size)
+        if kwargs['type'] == 'half_0':
+            return np.array([True] * (size//2) + [False]*(size - size//2))
+        if kwargs['type'] == 'half_1':
+            return np.array([False] * (size//2) + [True]*(size - size//2))
         if kwargs['type'] == 'random':
             # return get_full_mask(size)
-            return np.random.rand(1,size)>0.5
+            return np.random.rand(1, size) > 0.5
     # default, includes 'empty'
     return np.array([False] * size)
+
+
+def get_mask_from_string(s: str):
+    """Returns numpy boolean array created from string representation"""
+    b = []
+    for c in s:
+        b.append(c == '1')
+    return np.array(b)
--- boolmask_experiment/test_boolean_mask.py
+++ boolmask_experiment/test_boolean_mask.py
@@ -7,7 +7,7 @@ import random
 import numpy as np
 
 from boolmask_experiment import boolean_mask
-from boolmask_experiment.deap_testing_on_boolean_mask import bool_and
+from boolmask_experiment.boolean_mask import get_mask_from_string
 
 MASK_SIZE = 16
 
@@ -57,21 +57,21 @@ class TestBooleanMasks(unittest.TestCase):
         self.assertFalse(np.logical_xor(act_mask_0, exp_mask).all())
         self.assertFalse(np.logical_xor(act_mask_1, np.logical_not(exp_mask)).all())
 
-    def test_boolean_operator_and_mask_repeatively(self):
-        mask_set = self.get_set()
-        print(mask_set)
-        # empty_mask = boolean_mask.get_mask(MASK_SIZE, type='empty')
-        # full_mask = boolean_mask.get_mask(MASK_SIZE, type='full')
-        # act_mask_0 = boolean_mask.get_mask(4, type='half_0')
-        # act_mask_1 = boolean_mask.get_mask(4, type='half_1')
-        b = bool_and(random.choice(mask_set), random.choice(mask_set))
-        print(b)
-        for i in range(10):
-            b = bool_and(b, random.choice(mask_set))
-            print(f'{i} -- {b}')
-
-        print(b)
-        self.fail()
+    # def test_boolean_operator_and_mask_repeatively(self):
+    #     mask_set = self.get_set()
+    #     print(mask_set)
+    #     # empty_mask = boolean_mask.get_mask(MASK_SIZE, type='empty')
+    #     # full_mask = boolean_mask.get_mask(MASK_SIZE, type='full')
+    #     # act_mask_0 = boolean_mask.get_mask(4, type='half_0')
+    #     # act_mask_1 = boolean_mask.get_mask(4, type='half_1')
+    #     b = bool_and(random.choice(mask_set), random.choice(mask_set))
+    #     print(b)
+    #     for i in range(10):
+    #         b = bool_and(b, random.choice(mask_set))
+    #         print(f'{i} -- {b}')
+    #
+    #     print(b)
+    #     self.fail()
 
 
     @staticmethod
@@ -81,3 +81,19 @@ class TestBooleanMasks(unittest.TestCase):
         act_mask_0 = boolean_mask.get_mask(MASK_SIZE, type='half_0')
         act_mask_1 = boolean_mask.get_mask(MASK_SIZE, type='half_1')
         return [empty_mask, full_mask, act_mask_0, act_mask_1]
+
+    def test_get_mask_from_string_using_1111(self):
+        full_mask = boolean_mask.get_mask(4, type='full')
+        np.testing.assert_array_equal(full_mask, get_mask_from_string("1111"))
+
+    def test_get_mask_from_string_using_0000(self):
+        empty_mask = boolean_mask.get_mask(4, type='empty')
+        np.testing.assert_array_equal(empty_mask, get_mask_from_string("0000"))
+
+    def test_get_mask_from_string_using_1100(self):
+        half_0_mask = boolean_mask.get_mask(4, type='half_0')
+        np.testing.assert_array_equal(half_0_mask, get_mask_from_string("1100"))
+
+    def test_get_mask_from_string_using_0011(self):
+        half_1_mask = boolean_mask.get_mask(4, type='half_1')
+        np.testing.assert_array_equal(half_1_mask, get_mask_from_string("0011"))
