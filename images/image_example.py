f5cca53829aae283f9697260d72550624fe76d8e
Stealthness <stevewestinlondon@gmail.com>
2020-02-28 13:46:06 +0000

    updating previous work with notes

--- images/image_example.py
+++ images/image_example.py
@@ -35,13 +35,17 @@ def display_plot(plt_img, plt_name, **kwargs):
     plt.show()
 
 
-def display_img(file_input):
-    file = file_input.replace('\\', '//')
+def display_img(image_file: str):
+    """
+    Displays the image from the given file location, and print out type, min/max values and shape properties of the
+    the image :param image_file: the string location of a image file
+    """
+    file = image_file.replace('\\', '//')
     img = image.imread(file);
     name = get_file_name(file)
     print(f'the image {name} is of type {img.dtype}')
     print(f'the image {name} has the min/max values {np.min(img):.4f}/{np.max(img):.4f}')
-    print(f'The image {name} has the shape {img.shape}')
+    print(f'The image {name} has the shape {img.shape}\n')
     display_plot(img, name)
 
 
@@ -54,25 +58,23 @@ display_img(WORK_ROOT+'RES\TestFolder\Cars\car1.jpg')
 # converting to greyscale
 c_img = image.imread('lenna.png')
 bw_img = skimage.color.rgb2gray(c_img);
-display_plot(bw_img,'bw car', cmap='gray')
+display_plot(bw_img, 'Black and White Car', cmap='gray')
 
 
-# messing about with scalers
+# messing about with scalars
 fig, (sub1, sub2)= plt.subplots(1,2)
 
 hist = np.hstack(bw_img.flatten())
 sub1.hist(hist)
-sub1.set_title('Original data unscaled')
+sub1.set_title('Original Data Unscaled')
 
 
 scaler = StandardScaler()
 # th
 bw_img = scaler.fit_transform(bw_img)
-print(f'the image BW Scaled Lenna is of type {bw_img.dtype}')
+print(f'The Grayscale Image of Lenna is of type {bw_img.dtype}')
 
 hist = np.hstack(bw_img.flatten())
 sub2.hist(hist)
-sub2.set_title('Scaled in Guassian distribution')
+sub2.set_title('Scaled with a Guassian Distribution')
 plt.show()
-
-# here we can see that the
--- iris/iris_sklearn_example.py
+++ iris/iris_sklearn_example.py
@@ -1,6 +1,8 @@
-'''
+"""
 This is the sklearn iris example from https://scikit-learn.org/stable/auto_examples/datasets/plot_iris_dataset.html
-'''
+
+Uses the Iris data set, which is a set flower measurements of three different types of flowers
+"""
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn import datasets
@@ -8,6 +10,7 @@ from sklearn.decomposition import PCA
 
 iris = datasets.load_iris()
 
+# we only use the first two columns of the data set
 X = iris.data[:, :2]
 y = iris.target
 
@@ -20,8 +23,7 @@ plt.figure(2, figsize=(8, 6))
 plt.clf()
 
 # Plot the training points
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1,
-            edgecolor='k')
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1, edgecolor='k')
 plt.xlabel('Sepal length')
 plt.ylabel('Sepal width')
 
@@ -33,10 +35,14 @@ plt.yticks(())
 # To getter a better understanding of interaction of the dimensions
 # plot the first three PCA dimensions
 fig = plt.figure(1, figsize=(8, 6))
+# add 3d axes
 ax = Axes3D(fig, elev=-150, azim=110)
+# transform the data using PCA
 X_reduced = PCA(n_components=3).fit_transform(iris.data)
-ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=y,
-           cmap=plt.cm.Set1, edgecolor='k', s=40)
+# plot the transformed dat onto a scatter graph
+ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=y, cmap=plt.cm.Set1, edgecolor='k', s=40)
+
+# adding labels and titles
 ax.set_title("First three PCA directions")
 ax.set_xlabel("1st eigenvector")
 ax.w_xaxis.set_ticklabels([])
@@ -45,4 +51,5 @@ ax.w_yaxis.set_ticklabels([])
 ax.set_zlabel("3rd eigenvector")
 ax.w_zaxis.set_ticklabels([])
 
+# show both plots
 plt.show()
