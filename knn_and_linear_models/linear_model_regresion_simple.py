f0be20b93a005276808037739c0490196b4bd499
Stealthness <stevewestinlondon@gmail.com>
2020-02-29 12:22:52 +0000

    linear regression example, incomplete

--- knn_and_linear_models/linear_model_regresion_simple.py
+++ knn_and_linear_models/linear_model_regresion_simple.py
@@ -1,21 +1,42 @@
 """
 The purpose of the file is simple demonstration of Linear Regression using sklearn functions
+
+TO DO - INCOMPLETE
 """
 from sklearn import linear_model
 import numpy as np
 import matplotlib.pyplot as plt
-print("start")
+# below code is required for 3d axes to be diplayed
+from mpl_toolkits import mplot3d
+
+print("Simple example 1")
+# Create some data
+x_data = np.array([[0, 0], [1, 1], [2, 2]])
+y_data = np.array([0, 1, 2])
+
+# create a linear regression model
 reg = linear_model.LinearRegression()
-x_data = np.array([[0, 0],[1, 1],[2, 2]])
-y_data = [0,1,2]
+# train it with the data
 reg.fit(x_data, y_data)
+
 print(reg.coef_)
 
-x_new = np.linspace(x.min, x_max,0 )
+x_min = [x_data[:,0].min(),x_data[:,1].min()]
+x_max =  [x_data[:,0].max(),x_data[:,1].max()]
+
+x_new = np.linspace(x_min, x_max, 100)
+y_new = np.linspace(y_data.min(), y_data.max(), 100)
 
 # plot scatter graph
 plt.scatter(x_data[:, 0], x_data[:, 1], marker='+', color='blue')
-plt.plot()
+plt.plot(x_new[:,0],x_new[:,1], color='green')
 plt.xticks()
+plt.show()
+
+print("simple example in 3d scatter")
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1, projection='3d')
+ax.scatter(x_data[:, 0], x_data[:, 1], y_data, marker='+')
+plt.plot(x_new[:,0],x_new[:,1], y_new, color='green')
 plt.show()
 print("end")
