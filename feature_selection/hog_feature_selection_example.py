4fc6237d469a9e91f62a92f5bc33b7f53d542752
Stealthness <stevewestinlondon@gmail.com>
2020-02-29 12:45:23 +0000

    improving HoG

--- feature_selection/hog_feature_selection_example.py
+++ feature_selection/hog_feature_selection_example.py
@@ -15,7 +15,7 @@ file = WORK_ROOT+'RES\TestFolder\Cars\car1.jpg'
 img = image.imread(file)
 
 # create a figure with 2 subplots
-fig, (sub1, sub2) = plt.subplots(1, 2)
+fig, [[sub1, sub2], [sub3, sub4]] = plt.subplots(2, 2)
 
 # add the original image to subplot 1
 sub1.imshow(img)
@@ -23,15 +23,36 @@ sub1.axis('off')
 sub1.set_title('Original Image of a Red Car')
 
 # create HoG
-fd, hog_img = hog(img, orientations=8, pixels_per_cell=(16, 16),
+fd, hog_img_1 = hog(img, orientations=8, pixels_per_cell=(16, 16),
                     cells_per_block=(1, 1), visualize=True, multichannel=True)
 
-# add the original image to subplot 1
-sub2.imshow(hog_img)
+# add the original image to subplot 2
+sub2.imshow(hog_img_1)
 sub2.axis('off')
 sub2.set_title('HoG of a Red Car')
+
+# create another HoG
+fd, hog_img_2 = hog(img, orientations=8, pixels_per_cell=(32, 32),
+                    cells_per_block=(1, 1), visualize=True, multichannel=True)
+
+# add the original image to subplot 3
+sub3.imshow(hog_img_2)
+sub3.axis('off')
+sub3.set_title('HoG of a Red Car')
+
+# create another HoG
+fd, hog_img_3 = hog(img, orientations=8, pixels_per_cell=(16,16),
+                    cells_per_block=(4, 4), visualize=True, multichannel=True)
+
+# add the original image to subplot 4
+sub4.imshow(hog_img_3)
+sub4.axis('off')
+sub4.set_title('HoG of a Red Car')
+
 
 # show figure
 fig.suptitle('Histogram of Gradients Example')
 fig.show()
+
+
 
